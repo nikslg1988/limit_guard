daa4686d6cdbbb041b1d722647c74bb4033e55bc
nik slg <barmagloth1976@gmail.com>
2025-11-19 20:03:34 +0300

    Remove hash.py

--- hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import os
-
-salt = os.urandom(16)
-
-password = input("Введите данные для хэширования: ")
-hash_psw = hashlib.sha256(salt + password.encode("utf-8")).hexdigest()
-print(hash_psw)
