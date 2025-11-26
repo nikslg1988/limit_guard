7f45f831e106776aa6f45c9b532c7017f6961bc4
nik slg <barmagloth1976@gmail.com>
2025-11-26 22:01:57 +0300

    начал класс Category

--- user.py
+++ user.py
@@ -7,8 +7,9 @@ class User:
     def __init__(self, email, password):
         self.__id = str(uuid.uuid4())
         self.email = email
-        self.salt = os.urandom(16)
-        self.password = password #TODO валидацию пароля
+        self.__salt = os.urandom(16)
+        self.categories = []
+        self.password = password # валидацию пароля
         
     @property
     def email(self):
@@ -26,14 +27,20 @@ class User:
     
     @password.setter
     def password(self, value):
-        if not re.match(r'^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$', value):
-            raise ValueError("Пароль должен содержать как минимум одну заглавную букву, одну цифру и один символ")
+        if not re.match(r'^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$', value) or len(value) < 8:
+            raise ValueError("Пароль должен содержать как минимум одну заглавную букву, одну цифру и один символ и быть больше или равен 8 символам")
         self.__password = self._hash_password(value)
     
     def _hash_password(self, raw_password):
         byte_password = raw_password.encode("utf-8")
-        hash_hex = hashlib.sha256(self.salt + byte_password).hexdigest()
+        hash_hex = hashlib.sha256(self.__salt + byte_password).hexdigest()
         return hash_hex
+             
+    def check_password(self, raw_password):  #TODO
+        hash_check = self._hash_password(raw_password)
+        return hash_check == self.__password
     
-    def _check_password(self, raw_password):  #TODO
-        pass
+    def add_category(self, category):
+        self.categories.append(category)
+    
+
